946a3973f0f4e8a14d5e28d76d00ae523184cfa5
sdanil-ops <dsmirnov@psystems.biz>
2021-06-12 17:20:55 +0300

    refactor: refactor task1.py code
    add HelloMessage class with test unit

--- week2/task1.py
+++ week2/task1.py
@@ -3,10 +3,26 @@
 #
 #    displays  "Hello, world!". You can paste any name against 'world'
 #    -----------------------------------------------------------
+from typing import Optional
 
-def say_hello(name: str = 'мир'):
-    """prints hello to name"""
-    print(f'Здравствуй, {name}!')
+class HelloMessage:
+    def __init__(self, recipient: str = None):
+        if recipient is not None:
+            self.recipient = recipient
+        else:
+            self.recipient = 'мир'
+
+    def __str__(self):
+        return f'Здравствуй, {self.recipient}!'
 
 
-say_hello()
+def test_hello_message(test: Optional[str] = None):
+    print('testing...', test)
+    test_case_passed = HelloMessage(test).__str__() == f'Здравствуй, {test}!' if test else 'Здравствуй, Мир!'
+    print('passed' if test_case_passed else 'fail')
+
+
+if __name__ == '__main__':
+    test_hello_message('Python')
+    test_hello_message('World')
+    test_hello_message()
