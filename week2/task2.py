23b925fb518c136d7531ca23b469eecaf8e3db69
sdanil-ops <dsmirnov@psystems.biz>
2021-06-12 19:06:02 +0300

    refactor: refactor task2.py code tests

--- week2/task2.py
+++ week2/task2.py
@@ -4,6 +4,9 @@
 #    with one space between them. Sequence is '4 8 15 16 23 42'
 #    You can use any sequence to print
 #    -----------------------------------------------------------
+from task1 import TestUnit
+
+
 class Sequence:
     def __init__(self, data: list):
         self.data = data
@@ -14,21 +17,6 @@ class Sequence:
 
     def __str__(self):
         return ' '.join(self.data)
-
-
-
-class TestUnit:
-    def __init__(self, _class, _input, _output):
-        self._class = _class
-        self._input = _input
-        self._output = _output
-        self.is_passed = self.is_passed_test()
-        print('testing...', end=' ')
-
-    def is_passed_test(self):
-        test = self._class(self._input)
-        return test.__str__() == self._output
-
 
 
 if __name__ == '__main__':
