cb5f7fa0a738611395da5cbe383efddb344749a9
sdanil-ops <dsmirnov@psystems.biz>
2021-06-12 18:53:24 +0300

    refactor: refactor task3.py code
    add Sequence class with test class

--- week2/task3.py
+++ week2/task3.py
@@ -1,15 +1,36 @@
 #    -----------------------------------------------------------
 #    Copyright (c) 2021. Danil Smirnov
-#    Modify the previous program so that each number
-#    in the sequence is printed on a separate line.
+#    Write a program that prints a sequence of numbers
+#    with one space between them. Sequence is '4 8 15 16 23 42'
+#    You can use any sequence to print
 #    -----------------------------------------------------------
+class Sequence:
+    def __init__(self, data: list):
+        self.data = data
 
-def print_sequence(sequence: list = None):
-    """Prints sequence of numbers each one on separate line"""
-    if sequence is None:
-        sequence = ['4', '8', '15', '16', '23', '42']
-    for element in sequence:
-        print(element)
+    def _print(self):
+        for element in self.data:
+            print(element, end='\n')
+
+    def __str__(self):
+        return ' '.join(self.data)
 
 
-print_sequence()
+
+class TestUnit:
+    def __init__(self, _class, _input, _output):
+        self._class = _class
+        self._input = _input
+        self._output = _output
+        self.is_passed = self.is_passed_test()
+        print('testing...', end=' ')
+
+    def is_passed_test(self):
+        test = self._class(self._input)
+        return test.__str__() == self._output
+
+
+
+if __name__ == '__main__':
+    sequence = Sequence(['4', '8', '15', '16', '23', '42'])
+    sequence._print()
