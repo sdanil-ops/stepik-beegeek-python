6f15b04c01e60a7652b15e47ddecec50ffeda70f
sdanil-ops <dsmirnov@psystems.biz>
2021-06-12 17:54:45 +0300

    refactor: refactor task2.py code
    add Sequence class with test class

--- week2/task2.py
+++ week2/task2.py
@@ -4,14 +4,27 @@
 #    with one space between them. Sequence is '4 8 15 16 23 42'
 #    You can use any sequence to print
 #    -----------------------------------------------------------
+class Sequence:
+    def __init__(self, data: list):
+        self.data = data
+
+    def __str__(self):
+        return ' '.join(self.data)
 
 
-def print_sequence(sequence: list = None):
-    """Prints sequence of numbers with one space between them"""
-    if sequence is None:
-        sequence = ['4', '8', '15', '16', '23', '42']
-    for element in sequence:
-        print(element, end=' ')
+class TestUnit:
+    def __init__(self, _class, _input, _output):
+        self._class = _class
+        self._input = _input
+        self._output = _output
+        self.is_passed = self.is_passed_test()
+        print('test...', end=' ')
+
+    def is_passed_test(self):
+        test_case = self._class(self._input)
+        return test_case.__str__() == self._output
 
 
-print_sequence()
+if __name__ == '__main__':
+    test = TestUnit(Sequence, ['4', '8', '15', '16', '23', '42'], '4 8 15 16 23 42')
+    print('passed' if test.is_passed else 'failed')
