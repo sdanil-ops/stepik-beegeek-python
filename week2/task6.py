4fe1dbff1d1d7df562733592153728eed0e27e3d
sdanil-ops <dsmirnov@psystems.biz>
2021-06-13 11:10:48 +0300

    refactor: refactor code at task6.py, add test unit

--- week2/task6.py
+++ week2/task6.py
@@ -4,12 +4,34 @@
 #    the football team. The program repeats it on the screen
 #    with the words "- чемпион!" Uses similar to task1 code.
 #    -----------------------------------------------------------
+class TeamChampion:
+    def __init__(self, name: str):
+        self.name = name
 
-def praise_team(team: str = None):
-    """Prints praise to specified team"""
-    if team is None:
-        team = input()
-    print(f'{team} - чемпион!')
+    def praise(self):
+        return f'{self.name} - чемпион!'
+
+    def __str__(self):
+        return f'{self.name} - чемпион!'
 
 
-praise_team()
+class TestUnit:
+    def __init__(self, _class, _input, _output):
+        self._class = _class
+        self._input = _input
+        self._output = _output
+        self.is_passed = self.is_passed_test()
+        print('testing...', self._class)
+
+    def is_passed_test(self):
+        test = self._class(self._input)
+        return test.praise() == self._output
+
+
+if __name__ == '__main__':
+    team = TeamChampion(input())
+    print(team)
+
+# if __name__ == '__main__':
+#     test = TestUnit(TeamChampion, 'Барселона', 'Барселона - чемпион!')
+#     print('passed' if test.is_passed else 'failed')
