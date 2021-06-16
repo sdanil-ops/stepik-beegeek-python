d6a2013f01b7daa3eeb64b038a4f23913075cccb
sdanil-ops <dsmirnov@psystems.biz>
2021-06-16 16:48:07 +0300

    refactor: refactor task15.py code, add test unit

--- week2/task15.py
+++ week2/task15.py
@@ -3,9 +3,26 @@
 #    f(a,b) =3(a+b)**3 + 275b**2 − 127a−41
 #    program for calculating the value of a function
 #    -----------------------------------------------------------
-def calculate_strange_function(a: int, b: int) -> int:
-    """Calculates value of a function f(a,b) =3(a+b)**3 + 275b**2 − 127a−41"""
-    return (3 * ((a + b) * (a + b) * (a + b))) + (275 * (b * b)) - (127 * a) - 41
+class StrangeFunction:
+    def __init__(self, value_a: int, value_b: int):
+        self.value_a = value_a
+        self.value_b = value_b
+        self.value = self.calculate()
+
+    def calculate(self):
+        return (3 * ((self.value_a + self.value_b) * (self.value_a + self.value_b) * (self.value_a + self.value_b))) + \
+               (275 * (self.value_b * self.value_b)) - (127 * self.value_a) - 41
 
 
-print(calculate_strange_function(int(input()), int(input())))
+# --------------- - - test - - ----------------
+# if __name__ == '__main__':
+#     print('testing: ', StrangeFunction.calculate)
+#     func = StrangeFunction(1, 1)
+#     test_is_passed = func.value == 131
+#     print('passed' if test_is_passed else 'failed')
+
+
+# --------------- - - run!  - - ------------------
+if __name__ == '__main__':
+    my_function = StrangeFunction(int(input()), int(input()))
+    print(my_function.value)
