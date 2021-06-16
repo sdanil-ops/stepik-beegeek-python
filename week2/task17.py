1aa1df9b734156292e4ded381776263903d94787
sdanil-ops <dsmirnov@psystems.biz>
2021-06-16 17:24:14 +0300

    refactor: refactor task17.py code, add test unit

--- week2/task17.py
+++ week2/task17.py
@@ -4,13 +4,34 @@
 #    consisting of a monitor, system unit, keyboard and mouse.
 #
 #    -----------------------------------------------------------
-def calculate_price_of_set(components: int = 4, count: int = 3) -> int:
-    """calculates full price of given set of components"""
-    result = 0
-    for i in range(components):
-        result += int(input())
-    return result * count
+from typing import List
+from task1 import TestUnit
 
 
-print(calculate_price_of_set())
+class PCSet:
+    def __init__(self, prices: List[int]):
+        self.prices = prices
+        self.total = self.calculate_total()
 
+    def calculate_total(self):
+        result = 0
+        for price in self.prices:
+            result += price
+        return result
+
+
+class PCSetCost(PCSet):
+    def __str__(self):
+        return self.total
+
+
+# ------------ testing --------------
+# if __name__ == '__main__':
+#     test = TestUnit(PCSetCost, [1, 2, 3, 4], 10)
+#     print('passed' if test.is_passed else 'failed')
+
+
+# ------------ running --------------
+if __name__ == '__main__':
+    pc_set_cost = PCSetCost([int(input()) for _ in range(4)])
+    print(pc_set_cost.total * 3)
