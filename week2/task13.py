8fcb77eff686d063ea1b725cc1a6a0ff94b3d4c5
sdanil-ops <dsmirnov@psystems.biz>
2021-06-16 16:11:59 +0300

    refactor: refactor task13.py code, add test unit

--- week2/task13.py
+++ week2/task13.py
@@ -3,12 +3,29 @@
 #    program reads three integers and displays their sum.
 #    Each number is written on a separate line.
 #    -----------------------------------------------------------
-def calculate_sum(count: int = 3):
-    """Calculates sum of inputted numbers"""
-    result = 0
-    for i in range(count):
-        result += int(input())
-    return result
+from typing import List
 
 
-print(calculate_sum())
+class ThreeNumbers:
+    def __init__(self, data: List[int]):
+        self.data = data
+        self.sum_of_elements = self.calculate_sum_of_elements()
+
+    def calculate_sum_of_elements(self):
+        result = 0
+        for elem in self.data:
+            result += elem
+        return result
+
+
+# ---------- test -------------
+# if __name__ == '__main__':
+#     print('testing...', ThreeNumbers)
+#     test_is_passed = ThreeNumbers([1, 2, 3]).sum_of_elements == 6
+#     print('passed' if test_is_passed else 'failed')
+
+
+#----------- run --------------
+if __name__ == '__main__':
+    three_numbers = ThreeNumbers([int(input()) for _ in range(3)])
+    print(three_numbers.sum_of_elements)
