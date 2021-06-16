92cff089080d2857589a7b334b748d6ab31496ce
sdanil-ops <dsmirnov@psystems.biz>
2021-06-16 18:53:39 +0300

    refactor: refactor task22.py code, add test unit

--- week2/task22.py
+++ week2/task22.py
@@ -3,9 +3,31 @@
 #    program finds  number of meters for
 #    a given number of centimeters.
 #    -----------------------------------------------------------
-def convert_to_meters(sm: int) -> int:
-    """converts centimeters to meters"""
-    return sm // 100
+class Distance:
+    def __init__(self, meters: int = None, centimeters: int = None):
+        if meters:
+            self.meters = meters
+            self.centimeters = self.convert_to_centimeters()
+        elif centimeters:
+            self.centimeters = centimeters
+            self.meters = self.convert_to_meters()
+
+    def convert_to_meters(self):
+        return self.centimeters // 100
+
+    def convert_to_centimeters(self):
+        return self.meters * 100
 
 
-print(convert_to_meters(int(input())))
+# ------------ testing ------------
+if __name__ == '__main__':
+    test1_is_passed = Distance(centimeters=345).meters == 3
+    print('passed test 1' if test1_is_passed else 'failed test 1')
+    test2_is_passed = Distance(meters=3).centimeters == 300
+    print('passed test 2' if test2_is_passed else 'failed test 2')
+
+
+# ------------ running  ------------
+# if __name__ == '__main__':
+#     distance = Distance(centimeters=int(input()))
+#     print(distance.meters)
