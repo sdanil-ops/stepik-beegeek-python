385dbfacf9b5fc86bc34b381ed03293ae293d1c0
sdanil-ops <dsmirnov@psystems.biz>
2021-06-16 18:12:45 +0300

    refactor: refactor task20.py code, add test unit

--- week2/task20.py
+++ week2/task20.py
@@ -3,13 +3,20 @@
 #    program that reads a positive integer x
 #    and displays a sequence of numbers x, x2, x3, x4, x5
 #    -----------------------------------------------------------
-def get_strange_sequence(number: int, lentht: int = 5, separator: str = '---') -> str:
-    result = ''
-    for i in range(1, lentht + 1):
-        result += str(number * i)
-        if i < lentht:
-            result += separator
-    return result
+from task10 import StringsWithSeparator
+from task1 import TestUnit
+
+# --------- testing ----------
+# if __name__ == '__main__':
+#     test = TestUnit(StringsWithSeparator, ['7', '14', '21', '28', '35'], '7***14***21***28***35')
+#     print('passed' if test.is_passed else 'failed')
 
 
-print(get_strange_sequence(int(input())))
+# -------- running ----------
+if __name__ == '__main__':
+    value = int(input())
+    sequence = [value * i for i in range(1, 6)]
+    for i in range(len(sequence)):
+        sequence[i] = str(sequence[i])
+    result = StringsWithSeparator(sequence, separator='---')
+    print(result)
