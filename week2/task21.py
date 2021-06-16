e8f0301474d925d09ea270c374c4640c5d9fc110
sdanil-ops <dsmirnov@psystems.biz>
2021-06-16 18:53:22 +0300

    refactor: refactor task21.py code, add test unit

--- week2/task21.py
+++ week2/task21.py
@@ -3,9 +3,28 @@
 #    Program receives three integers as input: a1, d and n
 #    Program should output  the nth term of the geometric progression.
 #    -----------------------------------------------------------
-def get_n_term_of_geometric_progression(first: int, step: int, n: int) -> int:
-    """returns n term of geometric progression starting from the first with a given step"""
-    return first * (step ** (n - 1))
+from task1 import TestUnit
 
 
-print(get_n_term_of_geometric_progression(int(input()), int(input()), int(input())))
+class GeometricProgression:
+    def __init__(self, first: int, step: int):
+        self.first = first
+        self.step = step
+
+
+class TermOfProgression(GeometricProgression):
+    def get_n_term(self, n: int):
+        """returns n term of geometric progression"""
+        return self.first * (self.step ** (n - 1))
+
+
+# ------------ testing ------------
+# if __name__ == '__main__':
+#     test = TestUnit(TermOfProgression(1, 2).get_n_term, 5, '16')
+#     print('passed' if test.is_passed else 'failed')
+
+
+# ------------ running ------------
+if __name__ == '__main__':
+    geometric_progression_term = TermOfProgression(int(input()), int(input())).get_n_term(int(input()))
+    print(geometric_progression_term)
