50f0c13f5d5a5a4aa9abd56d4684abae62301634
sdanil-ops <dsmirnov@psystems.biz>
2021-07-15 11:45:25 +0300

    refactor: refactor task17.py code, add tests

--- week4/task17.py
+++ week4/task17.py
@@ -6,16 +6,39 @@
 from typing import Tuple
 
 
-def is_king_move(x: Tuple[int, int], y: Tuple[int, int]) -> bool:
-    """returns True if king can move from x to y"""
-    return -1 <= x[0] - y[0] <= 1 and -1 <= x[1] - y[1] <= 1
+class Position:
+
+    def __init__(self, coordinates: Tuple[int, int]):
+        self.x = coordinates[0]
+        self.y = coordinates[1]
 
 
-def check_king_move(x: Tuple[int, int], y: Tuple[int, int]) -> str:
-    if is_king_move(x, y):
-        return 'YES'
-    return 'NO'
+class KingMove:
+
+    def __init__(self, start_position: Position, end_position: Position):
+        self.start_position = start_position
+        self.end_position = end_position
+
+    def is_possible_king_move(self):
+        return -1 <= self.start_position.x - self.end_position.x <= 1 and \
+               -1 <= self.start_position.y - self.end_position.y <= 1
 
 
 if __name__ == '__main__':
-    print(check_king_move((int(input()), int(input())), (int(input()), int(input()))))
+    # --------- testing ---------
+    # tests = {
+    #     'test 1': KingMove(Position((4, 4)), Position((5, 5))).is_possible_king_move() is True,
+    #     'test 2': KingMove(Position((4, 4)), Position((5, 4))).is_possible_king_move() is True,
+    #     'test 3': KingMove(Position((4, 4)), Position((2, 4))).is_possible_king_move() is False,
+    # }
+    # flag = True
+    # for test in tests:
+    #     print(f'passed {test}' if tests[test] else f'failed {test}')
+    #     if not tests[test]:
+    #         flag = False
+    # print('all tests was passed' if flag else 'not all tests was passed')
+
+    # --------- running ---------
+    move = KingMove(Position((int(input()), int(input()))), Position((int(input()), int(input()))))
+    print('YES' if move.is_possible_king_move() else 'NO')
+
