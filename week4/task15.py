fe4e59490dddac106a13ad1e9c6429f5e1089550
sdanil-ops <dsmirnov@psystems.biz>
2021-07-15 09:57:46 +0300

    refactor: refactor task15.py code, add tests

--- week4/task15.py
+++ week4/task15.py
@@ -4,15 +4,30 @@
 #    is a leap year. If the year is a leap year,
 #    then output "YES", otherwise output "NO".
 #    -----------------------------------------------------------
-def is_leap(year: int) -> bool:
-    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+class Year:
+    def __init__(self, year: int):
+        self.year = year
+        self.is_leap = self.is_leap()
 
+    def is_leap(self) -> bool:
+        return (self.year % 4 == 0 and self.year % 100 != 0) or (self.year % 400 == 0)
 
-def check_year(year: int) -> str:
-    if is_leap(year):
-        return 'YES'
-    return 'NO'
+    def __repr__(self):
+        return 'YES' if self.is_leap else 'NO'
 
 
 if __name__ == '__main__':
-    print(check_year(int(input())))
+    # ---------- testing ----------
+
+    tests = {
+        'test 1': Year(2020).is_leap is True,
+        'test 2': Year(2012).is_leap is True,
+        'test 3': Year(2009).is_leap is not True
+    }
+    for test in tests:
+        print(f'passed {test}' if tests[test] else f'failed {test}')
+
+    # ---------- running ----------
+
+    # year = Year(int(input()))
+    # print(year)
